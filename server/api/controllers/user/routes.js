3e0fc276f30eac250d09c540c9483c6683397e87
Ubuntu <ubuntu@ip-172-31-1-227.ec2.internal>
2025-08-26 12:57:43 +0000

    update code

--- server/api/controllers/user/routes.js
+++ server/api/controllers/user/routes.js
@@ -9,10 +9,10 @@ export default Express.Router()
 
     .use(auth.verifyToken)
 
-    .post('/create', auth.checkRole(userRole.user.create), controller.create)
-    .get('/view', auth.checkRole(userRole.user.view), controller.view)
-    .get('/list', auth.checkRole(userRole.user.list), controller.list)
-    .put('/update', auth.checkRole(userRole.user.update), controller.update)
-    .delete('/delete', auth.checkRole(userRole.user.delete), controller.delete)
+    .post('/create', controller.create)
+    .get('/view', controller.view)
+    .get('/list', controller.list)
+    .put('/update', controller.update)
+    .delete('/delete', controller.delete)
 
 
